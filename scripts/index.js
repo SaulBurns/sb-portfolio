d78eaa0574816bfca480ff94ec22fb24f9e22230
saburns1 <saulburnss@gmail.com>
2026-01-02 22:06:56 -0700

    Add mid-section

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+const typed = new Typed('#js-dynamic-headline', {
+  strings: [
+    'Software Engineer',
+    'College Student',
+    'AI/ML Enthusiast',
+    'Problem Solver',
+    'Random Guy'
+  ],
+  typeSpeed: 70,
+  backSpeed: 30,
+  backDelay: 2000,
+  loop: true
+});
