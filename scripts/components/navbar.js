92a152aebf41914fb69c36eedb331a131a82eff3
saburns1 <saulburnss@gmail.com>
2026-01-14 20:36:43 -0700

    Add full responsiveness for small tablets and phones

--- scripts/components/navbar.js
+++ scripts/components/navbar.js
@@ -1,16 +1,23 @@
 const navbar = document.getElementById('js-nav-links');
 
-const media = window.matchMedia("(width < 700px)");
+const media = window.matchMedia("(width < 768px)");
+
+const picTwo = document.getElementById('js-picture-two');
+
+const picThree = document.getElementById('js-picture-three');
 
 media.addEventListener('change', (e) => updateNavbar(e))
 
 function updateNavbar(e) {
   const isMobile = e.matches;
-  console.log(isMobile);
   if(isMobile) {
     navbar.setAttribute('inert', '');
+    picTwo.setAttribute('src', 'images/me/third-photo.jpg');
+    picThree.setAttribute('src', 'images/me/second-photo.jpg');
   } else {
     navbar.removeAttribute('inert');
+    picTwo.setAttribute('src', 'images/me/second-photo.jpg');
+    picThree.setAttribute('src', 'images/me/third-photo.jpg');
   }
 }
 
