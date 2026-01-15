bed47ac60278b2a7e24bff14087ca6b09f05a9fb
saburns1 <saulburnss@gmail.com>
2026-01-14 23:50:07 -0700

    Fix script structure, and minimal enhacements in the index page

--- scripts/components/navbar.js
+++ scripts/components/navbar.js
@@ -2,22 +2,14 @@ const navbar = document.getElementById('js-nav-links');
 
 const media = window.matchMedia("(width < 768px)");
 
-const picTwo = document.getElementById('js-picture-two');
-
-const picThree = document.getElementById('js-picture-three');
-
 media.addEventListener('change', (e) => updateNavbar(e))
 
 function updateNavbar(e) {
   const isMobile = e.matches;
   if(isMobile) {
     navbar.setAttribute('inert', '');
-    picTwo.setAttribute('src', 'images/me/third-photo.jpg');
-    picThree.setAttribute('src', 'images/me/second-photo.jpg');
   } else {
     navbar.removeAttribute('inert');
-    picTwo.setAttribute('src', 'images/me/second-photo.jpg');
-    picThree.setAttribute('src', 'images/me/third-photo.jpg');
   }
 }
 
